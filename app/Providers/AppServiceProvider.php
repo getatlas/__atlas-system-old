4f71f722aea6f0eae016ff99ab7bd7ad7de99e89
Alex Bowers <bowersbros@gmail.com>
2016-03-25 21:15:43 +0000

    Allow @has

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,23 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot()
     {
-        //
+        Blade::directive('has', function ($expression) {
+            return "<?php if(!empty(trim(\$__env->yieldContent{$expression}))): ?>";
+        });
     }
 
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register()
     {
