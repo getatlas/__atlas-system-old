f3eb7daef436121616880f63125abbfd11a3255c
Alex Bowers <bowersbros@gmail.com>
2016-03-25 16:11:13 +0000

    laravel ide helper

--- config/app.php
+++ config/app.php
@@ -156,6 +156,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * For generating the IDE helper file
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
