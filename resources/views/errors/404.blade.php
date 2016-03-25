8b166b89a1ddd93c85efd23d3e16a3e3e8c7e4dd
Alex Bowers <bowersbros@gmail.com>
2016-03-25 22:42:48 +0000

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.error')
+
+<main role="main">
+    <article>
+        <div class="padding-t2">
+            <div class="row">
+                <div class="large-6 medium-8 small-11 columns small-centered text-invert text-center">
+
+                    <!-- logo -->
+                    <div class="padding-4 opacity-3 text-center">
+                        <div class="row">
+                            <div class="small-4 columns small-centered">
+                                <img src="{{ elixir('img/atlas-mark-128.png') }}" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <h1 class="margin-none text-white">404 Error</h1>
+                    <p class="margin-none-t text-big">The page you're looking for can't be found</p>
+                    <a class="full button-small color-bg-primary border-solid text-invert text-center text-decoration-none text-weight-700 rounded" href="#">Return home</a>
+                </div>
+            </div>
+        </div>
+    </article>
+</main>
